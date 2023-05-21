acd8538b41dfc84e7779d9c0406c72e1c576ba33
Seongeun Kim <kse3447@gmail.com>
2023-05-21 13:53:52 +0900

    CSS study

--- colors.js
+++ colors.js
@@ -1,37 +1,37 @@
 var Links = {
-    setColor: function (color) {
-        var alist = document.querySelectorAll("a");
-        var i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i = i + 1;
-        }
-    },
+  setColor: function (color) {
+    var alist = document.querySelectorAll("a");
+    var i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = color;
+      i = i + 1;
+    }
+  },
 };
 
 var Body = {
-    //객체에 속해있는 함수는 메서드라고 부른다!!!
-    setColor: function (color) {
-        document.querySelector("body").style.color = color;
-    },
-    setBackgroundColor: function (color) {
-        document.querySelector("body").style.backgroundColor = color;
-    },
+  //객체에 속해있는 함수는 메서드라고 부른다!!!
+  setColor: function (color) {
+    document.querySelector("body").style.color = color;
+  },
+  setBackgroundColor: function (color) {
+    document.querySelector("body").style.backgroundColor = color;
+  },
 };
 
 function nightDayHandler(self) {
-    var target = document.querySelector("body");
-    if (self.value === "Night") {
-        Body.setBackgroundColor("black");
-        Body.setColor("white");
-        self.value = "Day";
+  var target = document.querySelector("body");
+  if (self.value === "Night") {
+    Body.setBackgroundColor("black");
+    Body.setColor("white");
+    self.value = "Day";
 
-        Links.setColor("yellow");
-    } else {
-        Body.setBackgroundColor("white");
-        Body.setColor("black");
-        self.value = "Night";
+    Links.setColor("yellow");
+  } else {
+    Body.setBackgroundColor("white");
+    Body.setColor("black");
+    self.value = "Night";
 
-        Links.setColor("blue");
-    }
+    Links.setColor("blue");
+  }
 }
